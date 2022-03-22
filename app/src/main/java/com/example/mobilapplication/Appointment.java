818dfe0397d80b43f79adea699696c5be22487c9
András Dömösi <sz0domand@vasvari.org>
2022-03-22 09:03:29 +0100

    UserMenu layout kész

--- app/src/main/java/com/example/mobilapplication/Appointment.java
+++ app/src/main/java/com/example/mobilapplication/Appointment.java
@@ -65,7 +65,7 @@ public class Appointment extends AppCompatActivity {
                             String[] data = new String[2];
                             data[0] = day;
                             data[1] = hour;
-                            PutData putData = new PutData("http://10.0.11.106/kutyakozmetikaphp/order.php", "POST", field, data);
+                            PutData putData = new PutData("http://10.0.11.107/kutyakozmetikaphp/order.php", "POST", field, data);
                             if (putData.startPut()) {
                                 if (putData.onComplete()) {
                                     String result = putData.getResult();
---
+++ app/src/main/java/com/example/mobilapplication/UserMenu.java
@@ -0,0 +1,14 @@
+package com.example.mobilapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_menu);
+    }
+}
