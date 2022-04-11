b78a389157a63f2a1f9fc7fac46f470f99899879
Dömösi András <77957729+domosi-andris@users.noreply.github.com>
2022-04-11 18:26:40 +0200

    Add files via upload

---
+++ kutyakozmetikaphp/dogsignup.php
@@ -0,0 +1,11 @@
+<?php
+require "DataBase.php";
+$db = new DataBase();
+if (isset($_POST['agresszivE']) && isset($_POST['szorzetTipus'])&& isset($_POST['eletkor']) &&  isset($_POST['fajta']) && isset($_POST['tulajNev'])) {
+    if ($db->dbConnect()) {
+        if ($db->dogsignUp("kutya", $_POST['agresszivE'], $_POST['szorzetTipus'], $_POST['eletkor'], $_POST['fajta'], $_POST['tulajNev'])) {
+            echo "Sign Up Success";
+        } else echo "Sign up Failed";
+    } else echo "Error: Database connection";
+} else echo "All fields are required";
+?>
